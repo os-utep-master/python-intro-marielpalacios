d582c9fc9fd33d3a51f8cca9c3a3251223a301d3
marielpalacios <palaciosmariel18@gmail.com>
2019-09-01 20:11:23 -0600

    testing file reading and parsing

--- wordCount.py
+++ wordCount.py
@@ -1,1 +1,45 @@
-print('hello world.')
+#Mariel Palacios 
+#Python-intro-lab1
+#Theory of OS
+#Fall 2019
+
+import re
+import sys
+import os
+import subprocess
+
+# set input and output files
+if len(sys.argv) is not 3:
+    print("Correct usage: wordCount.py <input text file> <output file> " )
+    exit()
+
+inputFname = sys.argv[1]
+outputFname = sys.argv[2]
+#inputFname = sys.argv[3]
+
+wordList = list()
+
+# attempt to open input file
+with open(inputFname, 'r') as inputFile:
+    for line in inputFile:
+        # get rid of newline characters
+        line = line.strip()
+
+
+#line = "   a this is an example of a line. yes no.             ."
+        print(len(line))
+        print("line: " + line)
+
+
+        print("stripped: " + line.strip())
+
+        print("split: ")
+        wordsInLine = re.split('[ \t]', line)
+
+        print("wordsInLine:")
+        print(wordsInLine)
+
+        wordList.append(wordsInLine)
+        
+        print("wordList: ")
+        print(wordList)
